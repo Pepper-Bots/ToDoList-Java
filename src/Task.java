b84b2e1f6b322908a5d6efdb16dcbd5ad12e5fc6
Hélène Rizzon <helene.rizzon@live.fr>
2025-04-23 22:23:54 +0200

    Résolution de conflits : récupération des fichiers du stash

---
+++ src/Task.java
@@ -0,0 +1,71 @@
+package src;
+
+import java.util.Objects;
+
+
+// Définition de la classe Task (dans Task.java ou ici)
+public class Task {
+
+    protected final long id; // Identifiant unique de la tâche
+    protected String title; // Titre de la tâche
+    protected String description; // Description de la tâche
+    protected boolean done; // Indique si la tâche est terminée ou non
+    protected User creator; // Utilisateur qui a créé la tâche
+
+    // Constructeurs de la classe Task
+    public Task(String title, String description, User creator) {
+        this.id = Math.round(Math.random()* 1000); // Attribution de l'ID unique
+        this.title = title;
+        this.description = description;
+        this.done = false; // Par défaut, une tâche n'est pas terminée
+        this.creator = creator;
+    }
+    public Task(long id, String title, String description, User creator) {
+        this.id = Math.round(Math.random()* 1000);
+        this.title = title;
+        this.description = description;
+        this.creator = creator;
+    }
+    public Task(){
+        this.id = Math.round(Math.random()* 1000);
+    }
+
+
+    // Getters pour accéder aux attributs (encapsulation)
+    public final Long getId() { return id; }
+    public String getTitle() { return title; }
+    public String getDescription() { return description; }
+    public boolean isDone() { return done; }
+    public User getUser() { return creator; }
+
+    // Setters pour modifier les attributs
+    public void setTitle(String title) { this.title = title; }
+    public void setDescription(String description) { this.description = description; }
+    public void setDone(boolean done) { this.done = done; }
+
+
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj) return true;
+        if(obj == null || getClass() != obj.getClass()) return false;
+        src.Task task = (src.Task) obj;
+        return id == task.id;
+    }
+
+    @Override
+    public int hashCode(){ return Objects.hash(id); }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", done=" + done +
+                ", user=" + creator +
+                '}';
+    }
+
+
+}
+
